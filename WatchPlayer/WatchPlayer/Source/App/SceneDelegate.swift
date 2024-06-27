7dcb45b6f2377ec7d06acd1bd3512543e8c089e1
importants <dlwognsdk@tukorea.ac.kr>
2024-06-27 17:52:28 +0900

    feat: Coordinator > SceneDelegate 연결

--- WatchPlayer/WatchPlayer/Source/App/SceneDelegate.swift
+++ WatchPlayer/WatchPlayer/Source/App/SceneDelegate.swift
@@ -8,15 +8,24 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
+    var appFlowCoordinator: RootCoordinator?
+    let appDIContainer = AppDIContainer()
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
+        window?.backgroundColor = .systemBackground
         
-        let viewController = UIViewController()
-        window?.rootViewController = viewController
+        let navigationViewController = UINavigationController()
+        window?.rootViewController = navigationViewController
+        
+        appFlowCoordinator = .init(
+            navigationController: navigationViewController,
+            dependencies: appDIContainer
+        )
+        appFlowCoordinator?.start()
         window?.makeKeyAndVisible()
         
     }
