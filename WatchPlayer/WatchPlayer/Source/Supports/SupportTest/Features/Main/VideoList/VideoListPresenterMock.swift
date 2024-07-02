26cd41228bcb5cb014e4ccce2abcab52d2bb3f6b
Jae Hun Lee <118325810+importantsgit@users.noreply.github.com>
2024-07-03 00:30:39 +0900

    test: 테스트 코드 추가

---
+++ WatchPlayer/WatchPlayer/Source/Supports/SupportTest/Features/Main/VideoList/VideoListPresenterMock.swift
@@ -0,0 +1,16 @@
+//
+//  VideoListPresenter.swift
+//  WatchPlayer
+//
+//  Created by Importants on 7/2/24.
+//
+
+import Foundation
+
+final class VideoListPresenterMock: VideoListPresenterProtocol {
+    func showVideoLibrary() {
+        <#code#>
+    }
+    
+    
+}
---
+++ WatchPlayer/WatchPlayer/Source/Supports/SupportTest/Features/Main/VideoList/VideoListRouterMock.swift
@@ -0,0 +1,12 @@
+//
+//  VideoListRouter.swift
+//  WatchPlayer
+//
+//  Created by Importants on 7/2/24.
+//
+
+import Foundation
+
+final class VideoListRouterMock: VideoListRouterProtocol {
+    
+}
