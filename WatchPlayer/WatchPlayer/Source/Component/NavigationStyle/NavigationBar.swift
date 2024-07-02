9469cf9b7442d44b857aeb486db806be7c7f16e9
Jae Hun Lee <118325810+importantsgit@users.noreply.github.com>
2024-07-03 00:29:39 +0900

    feat: 네비게이션바 수정
    수정사항
    - Combine -> RxSwift로 변경
    - 필요없는 로직 제거

--- WatchPlayer/WatchPlayer/Source/Component/NavigationStyle/NavigationBar.swift
+++ WatchPlayer/WatchPlayer/Source/Component/NavigationStyle/NavigationBar.swift
@@ -6,22 +6,41 @@
 //
 
 import UIKit
-import Combine
+import RxSwift
+import RxRelay
 
-class NavigationBarView: UIView {
-    private let style: NavigationStyleType
-    private let bg: UIColor
-    private let isDisplayLeftButton: Bool
-    private let isDisplayRightButton: Bool
-    private let rightButtonType: NavigationButtonType
-    private let isOpenHiddenMenu: Bool
-    private var leftButtonAction = PassthroughSubject<Void, Never>()
-    private var rightButtonAction = PassthroughSubject<Void, Never>()
-    private var hiddenButtonAction = PassthroughSubject<Void, Never>()
+protocol NavigationBarProtocol {
+    func setTitle(
+        _ name: String
+    )
+    
+    func setAttriButeTitle(
+        _ name: NSAttributedString
+    )
+    
+    func setLeftButton(
+        imageName: String
+    ) -> Observable<Void>
+    
+    func setRightButton(
+        imageName: String
+    ) -> Observable<Void>
+    
+    func setHiddenButton(
+    ) -> Observable<Void>
+}
+
+final class NavigationBarView: UIView, NavigationBarProtocol {
+    private var leftButtonAction = PublishRelay<Void>()
+    private var rightButtonAction = PublishRelay<Void>()
+    private var hiddenButtonAction = PublishRelay<Void>()
+    
+    private let navigationBarColor: UIColor
     
     private var titleLabel: UILabel = {
         var label = UILabel()
         label.font = .systemFont(ofSize: 17, weight: .medium)
+        label.textColor = .title
         
         return label
     }()
@@ -32,21 +51,10 @@ class NavigationBarView: UIView {
     
     init(
         title: String = "",
-        style: NavigationStyleType = .dark,
-        backgroundColor: UIColor = .clear,
-        isDisplayLeftButton: Bool = false,
-        isDisplayRightButton: Bool = false,
-        rightButtonType: NavigationButtonType = .close,
-        isOpenHiddenMenu: Bool = false
+        navigationBarColor: UIColor = .clear
     ){
         self.titleLabel.text = title
-        self.style = style
-        self.bg = backgroundColor
-        self.isDisplayLeftButton = isDisplayLeftButton
-        self.isDisplayRightButton = isDisplayRightButton
-        self.rightButtonType = rightButtonType
-        self.isOpenHiddenMenu = isOpenHiddenMenu
-        
+        self.navigationBarColor = navigationBarColor
         
         super.init(frame: .zero)
         
@@ -58,46 +66,47 @@ class NavigationBarView: UIView {
     }
     
     @objc private func leftButtonTapped() {
-        leftButtonAction.send()
+        leftButtonAction.accept(())
     }
     
     @objc private func rightButtonTapped() {
-        rightButtonAction.send()
+        rightButtonAction.accept(())
     }
     
     @objc private func hiddenButtonTapped() {
-        hiddenButtonAction.send()
+        hiddenButtonAction.accept(())
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
-        if bg != .clear {
-            self.layer.shadowPath = UIBezierPath(
-                rect: CGRect(
-                    x: 0,
-                    y: 0,
-                    width: frame.width,
-                    height: frame.height
-                )).cgPath
-            self.backgroundColor = bg
-            //self.layer.shadowColor = UIColor.depth3.cgColor
-            self.layer.shadowOpacity = 0.1
-            self.layer.shadowRadius = 2
-            self.layer.shadowOffset = CGSize(width: 0, height: 2)
+        if navigationBarColor == .clear {
+            return
         }
+        
+        self.layer.shadowPath = UIBezierPath(
+            rect: CGRect(
+                x: 0,
+                y: 0,
+                width: frame.width,
+                height: frame.height
+            )).cgPath
+        self.backgroundColor = navigationBarColor
+        self.layer.shadowColor = UIColor.lightGray.cgColor
+        self.layer.shadowOpacity = 0.1
+        self.layer.shadowRadius = 2
+        self.layer.shadowOffset = CGSize(width: 0, height: 2)
     }
 }
 
 extension NavigationBarView {
     private func setupLayout() {
-    
+        
         [leftButton, titleLabel, rightButton, hiddenButton].forEach {
             $0.translatesAutoresizingMaskIntoConstraints = false
             self.addSubview($0)
             
             $0.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
         }
-        titleLabel.textColor = style == .light ? .white : .black
         
         NSLayoutConstraint.activate([
             leftButton.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 4),
@@ -116,68 +125,53 @@ extension NavigationBarView {
             rightButton.heightAnchor.constraint(equalToConstant: 48),
         ])
         
-        leftButton.isHidden = isDisplayLeftButton == false
-        rightButton.isHidden = isDisplayRightButton == false
-        hiddenButton.isHidden = isOpenHiddenMenu == false
+        leftButton.isHidden = true
+        rightButton.isHidden = true
+        hiddenButton.isHidden = true
         
-        if isOpenHiddenMenu {
-            hiddenButton.addTarget(self, action: #selector(hiddenButtonTapped), for: .touchUpInside)
-        }
     }
     
-    func setTitle(_ name: String) {
+    func setTitle(
+        _ name: String
+    ) {
         titleLabel.text = name
     }
     
-    func isDisplayLeftButton( _ isDisplay: Bool) {
-        leftButton.isHidden = isDisplay == false
-    }
-    
-    func isDisplayRightButton( _ isDisplay: Bool) {
-        rightButton.isHidden = isDisplay == false
+    func setAttriButeTitle(
+        _ name: NSAttributedString
+    ) {
+        titleLabel.attributedText = name
     }
     
     func setLeftButton(
-        imageName: String = "backButton_black",
-        _ isDisplayLeftButton: Bool? = nil
-    ) -> AnyPublisher<Void, Never> {
-        if let isDisplayLeftButton = isDisplayLeftButton,
-           isDisplayLeftButton == true {
-            leftButton.isHidden = false
-        }
-        
+        imageName: String = "backButton_black"
+    ) -> Observable<Void> {
+        leftButton.isHidden = false
         setLeftConfig(imageName: imageName)
         
         leftButton.addTarget(self, action: #selector(leftButtonTapped), for: .touchUpInside)
         return leftButtonAction
-            .eraseToAnyPublisher()
-    }
-    
-    func changeLeftImage(
-        imageName: String
-    ) {
-        leftButton.configuration?.image = UIImage(named: imageName)
+            .asObservable()
     }
     
     func setRightButton(
-        imageName: String,
-        _ isDisplayRightButton: Bool? = nil
-    ) -> AnyPublisher<Void, Never> {
-        if let isDisplayRightButton = isDisplayRightButton,
-            isDisplayRightButton == true {
-            rightButton.isHidden = false
-        }
-        
+        imageName: String
+    ) -> Observable<Void> {
+        rightButton.isHidden = false
         setRightConfig(imageName: imageName)
         
         rightButton.addTarget(self, action: #selector(rightButtonTapped), for: .touchUpInside)
         return rightButtonAction
-            .eraseToAnyPublisher()
+            .asObservable()
     }
     
-    func setHiddenEvent() -> AnyPublisher<Void, Never> {
-        hiddenButtonAction
-            .eraseToAnyPublisher()
+    func setHiddenButton(
+    ) -> Observable<Void> {
+        hiddenButton.isHidden = false
+        hiddenButton.addTarget(self, action: #selector(hiddenButtonTapped), for: .touchUpInside)
+        
+        return hiddenButtonAction
+            .asObservable()
     }
     
     private func setLeftConfig(imageName: String) {
