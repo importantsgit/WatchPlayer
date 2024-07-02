829fc738aef46bb52502aef2bec19e7dd166cb86
Jae Hun Lee <118325810+importantsgit@users.noreply.github.com>
2024-07-03 00:30:23 +0900

    feat: 동영상 리스트 뷰에 네이게이션바 추가

--- WatchPlayer/WatchPlayer/Source/Feature/Common/DefaultViewController.swift
+++ WatchPlayer/WatchPlayer/Source/Feature/Common/DefaultViewController.swift
@@ -6,12 +6,18 @@
 //
 
 import UIKit
+import RxSwift
 
 class DefaultViewController: UIViewController {
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupLayout()
+        setupNavigationBar()
     }
+    
+    func setupNavigationBar() {}
     
     func setupLayout() {
         view.backgroundColor = .systemBackground
--- WatchPlayer/WatchPlayer/Source/Feature/Main/VideoList/VideoList/View/VideoListViewController.swift
+++ WatchPlayer/WatchPlayer/Source/Feature/Main/VideoList/VideoList/View/VideoListViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 final class VideoListViewController: DefaultViewController {
     
+    let navigationBar: NavigationBarProtocol = NavigationBarView()
+    
     private let presenter: VideoListPresenterProtocol
     
     init(
@@ -20,5 +22,23 @@ final class VideoListViewController: DefaultViewController {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func setupLayout() {
+        super.setupLayout()
+    
+    }
+    
+    override func setupNavigationBar() {
+        navigationBar.setTitle("동영상 리스트")
+        navigationBar.setRightButton(imageName: "")
+            .subscribe(onNext: { [weak self] in
+                self?.presenter.showVideoLibrary()
+            })
+            .disposed(by: disposeBag)
     }
 }
