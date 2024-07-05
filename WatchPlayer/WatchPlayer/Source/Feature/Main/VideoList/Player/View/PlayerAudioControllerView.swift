569ab326da192f7bce4c6e96938b7d665eb75100
importants <dlwognsdk@tukorea.ac.kr>
2024-07-05 17:49:41 +0900

    feat: 오디오 뷰 추가

---
+++ WatchPlayer/WatchPlayer/Source/Feature/Main/VideoList/Player/View/PlayerAudioControllerView.swift
@@ -0,0 +1,17 @@
+//
+//  PlayerAudioControllerView.swift
+//  WatchPlayer
+//
+//  Created by 이재훈 on 7/5/24.
+//
+
+import UIKit
+
+enum SendFromAudioControllerEvent {
+    
+}
+
+
+final class PlayerAudioControllerView: UIView {
+    
+}
