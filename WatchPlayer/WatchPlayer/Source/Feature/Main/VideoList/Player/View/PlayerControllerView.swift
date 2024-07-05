39ae10926f9a113209ef6680f9f12b8be0923367
importants <dlwognsdk@tukorea.ac.kr>
2024-07-05 17:49:29 +0900

    refactor: PlayerControllerView에서 Event를 반환하거나 입력할 수 있는 Handler 추가 / 회전 시 레이아웃 변경되게 수정

--- WatchPlayer/WatchPlayer/Source/Feature/Main/VideoList/Player/View/PlayerControllerView.swift
+++ WatchPlayer/WatchPlayer/Source/Feature/Main/VideoList/Player/View/PlayerControllerView.swift
@@ -13,12 +13,30 @@ import RxSwift
 import RxGesture
 import RxCocoa
 
-protocol PlayerControllerViewProtocol {
+enum SendFromControllerEvent {
+    case playViewTapped
+    case controllerTapped
     
+    case audioButtonTapped
+    case settingButtonTapped
+    case rotationButtonTapped
+    case backButtonTapped
+    
+    case playButtonTapped
+}
+
+enum ReceiveByControllerEvent {
+    case setPlayButton(state: PlayerState)
+    case setTime(current: CMTime, duration: CMTime)
+    case setLayout(style: LayoutStyle)
+}
+
+protocol PlayerControllerViewProtocol: AnyObject {
+    func handleEvent(_ event: ReceiveByControllerEvent)
 }
 
 class PlayerControllerView: UIView {
-    private var presenter: PlayerControllerProtocol
+    weak var presenter: PlayerControllerProtocol?
     
     private var titleLabel: UILabel = {
         var label = UILabel()
@@ -28,26 +46,29 @@ class PlayerControllerView: UIView {
         return label
     }()
     
+    private let topControlView = UIStackView()
+    private let middleControlView = UIStackView()
+    private let bottomControlView = UIStackView()
+    private let seekbarView = UIView()
+    private let seekbar = UIView()
     private var backButton = UIButton()
     private var audioButton = UIButton()
     private var settingButton = UIButton()
-    
     private var playButton = UIButton()
-    
+    private var timeLabel = UILabel()
     private var rotationButton = UIButton()
     
     
     private var disposeBag = DisposeBag()
     
-    init(
-        presenter: PlayerControllerProtocol
-    ) {
-        self.presenter = presenter
+    private var layoutConstraints: [NSLayoutConstraint]?
+    
+    init() {
         super.init(frame: .zero)
         
         setupTappedGesture()
-        setupLayout()
         setupButtons()
+        setupLayout()
     }
     
     required init?(coder: NSCoder) {
@@ -58,53 +79,46 @@ class PlayerControllerView: UIView {
         self.rx.tapGesture()
             .when(.recognized)
             .subscribe(onNext: { [weak self] _ in
-                self?.presenter.controllerTapped()
+                self?.presenter?.handleContollerEvent(.controllerTapped)
             })
             .disposed(by: disposeBag)
     }
     
     private func setupButtons() {
-        let imageSize: CGFloat = 42
         [backButton, audioButton, settingButton, playButton, rotationButton].forEach{
             $0.configuration = .plain()
             $0.configuration?.imagePlacement = .all
         }
         
-        backButton.configuration?.image = UIImage(named: "back")?
-            .resized(to: .init(width: imageSize, height: imageSize))
         backButton.rx.tap.bind { [weak self] in
-            self?.presenter.backButtonTapped()
+            self?.presenter?.handleContollerEvent(.backButtonTapped)
         }
         .disposed(by: disposeBag)
-        
-        audioButton.configuration?.image = UIImage(named: "audio")?
-            .resized(to: .init(width: imageSize, height: imageSize))
+
         audioButton.rx.tap.bind { [weak self] in
-            self?.presenter.audioButtonTapped()
+            self?.presenter?.handleContollerEvent(.audioButtonTapped)
         }
         .disposed(by: disposeBag)
         
-        settingButton.configuration?.image = UIImage(named: "setting")?
-            .resized(to: .init(width: imageSize, height: imageSize))
         settingButton.rx.tap.bind { [weak self] in
-            self?.presenter.settingButtonTapped()
+            self?.presenter?.handleContollerEvent(.settingButtonTapped)
+        }
+        .disposed(by: disposeBag)
+        
+        playButton.rx.tap.bind { [weak self] in
+            guard let state = self?.presenter?.handleContollerEvent(.playButtonTapped) as? PlayerState
+            else { return }
+            self?.setPlayButton(state: state)
+        }
+        .disposed(by: disposeBag)
+        
+        rotationButton.rx.tap.bind { [weak self] in
+            self?.presenter?.handleContollerEvent(.rotationButtonTapped)
         }
         .disposed(by: disposeBag)
         
         playButton.configuration?.image = UIImage(named: "pause")?
-            .resized(to: .init(width: imageSize, height: imageSize))
-        playButton.rx.tap.bind { [weak self] in
-            self?.presenter.playButtonTapped()
-        }
-        .disposed(by: disposeBag)
-        
-        rotationButton.configuration?.image = UIImage(named: "expand")?
-            .resized(to: .init(width: imageSize, height: imageSize))
-        rotationButton.rx.tap.bind { [weak self] in
-            self?.presenter.expandButtonTapped()
-        }
-        .disposed(by: disposeBag)
-        
+            .resized(to: .init(width: 42, height: 42))
     }
     
     private func setupLayout() {
@@ -113,36 +127,30 @@ class PlayerControllerView: UIView {
         
         titleLabel.textAlignment = .center
         
-        let topControlView = UIStackView()
-        
-        
-        
         [backButton, titleLabel, audioButton, settingButton].forEach {
             $0.translatesAutoresizingMaskIntoConstraints = false
             topControlView.addArrangedSubview($0)
         }
-        
-        let middleControlView = UIStackView()
         
         [playButton].forEach {
             $0.translatesAutoresizingMaskIntoConstraints = false
             middleControlView.addArrangedSubview($0)
         }
         
-        let bottomControlView = UIStackView()
-        
-        let seekbarView = UIView()
-
         [seekbarView, rotationButton].forEach {
             $0.translatesAutoresizingMaskIntoConstraints = false
             bottomControlView.addArrangedSubview($0)
         }
         
-        let seekbar = UIView()
         seekbar.backgroundColor = .primary
         
-        seekbar.translatesAutoresizingMaskIntoConstraints = false
-        seekbarView.addSubview(seekbar)
+        timeLabel.textAlignment = .right
+        
+        [timeLabel, seekbar].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            seekbarView.addSubview($0)
+        }
+        
         seekbar.layer.cornerRadius = 2
         
         [topControlView, middleControlView, bottomControlView].forEach{
@@ -162,7 +170,31 @@ class PlayerControllerView: UIView {
             ])
         }
         
-        NSLayoutConstraint.activate([
+        layoutConstraints = setInitialLayout()
+         NSLayoutConstraint.activate(layoutConstraints!)
+    }
+    
+    private func setInitialLayout() -> [NSLayoutConstraint] {
+        if frame.width > frame.height {
+            return setLayoutToLandscape()
+        } else {
+            return setLayoutToPortrait()
+        }
+    }
+    
+    func setLayoutToPortrait(
+    ) -> [NSLayoutConstraint] {
+        let imageSize: CGFloat = 32
+        backButton.configuration?.image = UIImage(named: "back")?
+            .resized(to: .init(width: imageSize, height: imageSize))
+        audioButton.configuration?.image = UIImage(named: "audio")?
+            .resized(to: .init(width: imageSize, height: imageSize))
+        settingButton.configuration?.image = UIImage(named: "setting")?
+            .resized(to: .init(width: imageSize, height: imageSize))
+        rotationButton.configuration?.image = UIImage(named: "expand")?
+            .resized(to: .init(width: imageSize, height: imageSize))
+        
+        return [
             topControlView.topAnchor.constraint(equalTo: topAnchor, constant: 8),
             topControlView.leftAnchor.constraint(equalTo: leftAnchor, constant: 8),
             topControlView.rightAnchor.constraint(equalTo: rightAnchor, constant: -8),
@@ -176,14 +208,112 @@ class PlayerControllerView: UIView {
             
             seekbarView.heightAnchor.constraint(equalToConstant: 42),
             
+            timeLabel.topAnchor.constraint(equalTo: seekbarView.topAnchor),
+            timeLabel.leftAnchor.constraint(equalTo: seekbarView.leftAnchor),
+            
+            seekbar.topAnchor.constraint(equalTo: timeLabel.bottomAnchor, constant: -2),
             seekbar.bottomAnchor.constraint(equalTo: seekbarView.bottomAnchor, constant: -8),
             seekbar.leftAnchor.constraint(equalTo: seekbarView.leftAnchor),
             seekbar.rightAnchor.constraint(equalTo: seekbarView.rightAnchor),
             seekbar.heightAnchor.constraint(equalToConstant: 4),
-        ])
+        ]
     }
     
-    @objc func viewTapped(){
-        presenter.controllerTapped()
+    func setLayoutToLandscape(
+    ) -> [NSLayoutConstraint] {
+        let imageSize: CGFloat = 42
+        backButton.configuration?.image = UIImage(named: "back")?
+            .resized(to: .init(width: imageSize, height: imageSize))
+        audioButton.configuration?.image = UIImage(named: "audio")?
+            .resized(to: .init(width: imageSize, height: imageSize))
+        settingButton.configuration?.image = UIImage(named: "setting")?
+            .resized(to: .init(width: imageSize, height: imageSize))
+        rotationButton.configuration?.image = UIImage(named: "expand")?
+            .resized(to: .init(width: imageSize, height: imageSize))
+        
+        return [
+            topControlView.topAnchor.constraint(equalTo: topAnchor, constant: 16),
+            topControlView.leftAnchor.constraint(equalTo: leftAnchor, constant: 32),
+            topControlView.rightAnchor.constraint(equalTo: rightAnchor, constant: -32),
+            
+            middleControlView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            middleControlView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            
+            bottomControlView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -16),
+            bottomControlView.leftAnchor.constraint(equalTo: leftAnchor, constant: 32),
+            bottomControlView.rightAnchor.constraint(equalTo: rightAnchor, constant: -32),
+            
+            seekbarView.heightAnchor.constraint(equalToConstant: 42),
+            
+            timeLabel.topAnchor.constraint(equalTo: seekbarView.topAnchor),
+            timeLabel.leftAnchor.constraint(equalTo: seekbarView.leftAnchor),
+            
+            seekbar.topAnchor.constraint(equalTo: timeLabel.bottomAnchor, constant: -2),
+            seekbar.bottomAnchor.constraint(equalTo: seekbarView.bottomAnchor, constant: -8),
+            seekbar.leftAnchor.constraint(equalTo: seekbarView.leftAnchor),
+            seekbar.rightAnchor.constraint(equalTo: seekbarView.rightAnchor),
+            seekbar.heightAnchor.constraint(equalToConstant: 4),
+        ]
+    }
+    
+    func setPlayButton(state: PlayerState) {
+        let buttonImage: UIImage
+        switch state {
+        case .playing: buttonImage = .pause
+        case .paused: buttonImage = .play
+        case .ended: buttonImage = .rotate
+        }
+        
+        playButton.configuration?.image = buttonImage
+    }
+}
+
+extension PlayerControllerView: PlayerControllerViewProtocol {
+    func handleEvent(_ event: ReceiveByControllerEvent) {
+        switch event {
+        case .setLayout(let style):
+            self.setNeedsUpdateConstraints()
+            NSLayoutConstraint.deactivate(self.layoutConstraints ?? [])
+            
+            let newConstraints: [NSLayoutConstraint]
+            switch style {
+            case .landscape:
+                newConstraints = self.setLayoutToLandscape()
+            case .portrait:
+                newConstraints = self.setLayoutToPortrait()
+            }
+            
+            NSLayoutConstraint.activate(newConstraints)
+            self.layoutConstraints = newConstraints
+            
+            UIView.animate(withDuration: 0.3) {
+                self.layoutIfNeeded()
+            }
+            
+        case .setPlayButton(let state):
+            setPlayButton(state: state)
+            
+        case .setTime(current: let current, duration: let duration):
+            set(times: (current, duration))
+        }
+    }
+        
+    func set(
+        times: (current: CMTime, duration: CMTime)
+    ) {
+        timeLabel.attributedText = NSAttributedStringBuilder()
+            .add(
+                string: "\(times.current.convertCMTimeToString()) / ",
+                color: .white,
+                font: .systemFont(ofSize: 13, weight: .bold),
+                lineHeight: 14
+            )
+            .add(
+                string: "\(times.duration.convertCMTimeToString())",
+                color: .time,
+                font: .systemFont(ofSize: 13, weight: .medium),
+                lineHeight: 14
+            )
+            .buildNSAttributedString()
     }
 }
